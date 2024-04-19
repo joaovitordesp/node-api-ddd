cc1a9630f0751b2c841e51e59faa95bed0d92e74
jvitor.terceiro <jvitor.terceiro>
2024-04-19 16:57:46 -0300

    fix: changes all unit tests

--- src/domain/forum/application/use-cases/chooser-question-best.spec.ts
+++ src/domain/forum/application/use-cases/chooser-question-best.spec.ts
@@ -4,6 +4,7 @@ import { makeQuestion } from "test/factories/make-question";
 import { ChooseQuestionBestUseCase } from "./chooser-question-best";
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-question-repository";
 import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { NotAllowedError } from "./errors/not-allowed-error";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
@@ -52,11 +53,12 @@ describe("Choose Question Best Answer", () => {
     await inMemoryQuestionsRepository.create(question);
     await inMemoryAnswersRepository.create(answer);
 
-    expect(() => {
-      return sut.execute({
-        answerId: answer.id.toString(),
-        authorId: "author-2",
-      });
-    }).rejects.toBeInstanceOf(Error);
+    const result = await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: "author-2",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
